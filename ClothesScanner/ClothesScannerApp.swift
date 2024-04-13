c015d48a4e0b4fbbe58f840551f2ffaa40a847ea
Nicholas Kuo <nicholaskuo78@gmail.com>
2024-04-13 14:05:55 -0400

    Initial Commit

---
+++ ClothesScanner/ClothesScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesScannerApp.swift
+//  ClothesScanner
+//
+//  Created by Nicholas Kuo on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ClothesScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

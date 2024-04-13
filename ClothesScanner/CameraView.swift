f3b1766f6612c6eddbd9d99dd763e963cb21b99b
Nicholas Kuo <nicholaskuo78@gmail.com>
2024-04-13 14:28:07 -0400

    Initial camera changes

---
+++ ClothesScanner/CameraView.swift
@@ -0,0 +1,44 @@
+//
+//  CameraView.swift
+//  ClothesScannerTests
+//
+//  Created by Nicholas Kuo on 4/13/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: CameraView
+
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
---
+++ ClothesScanner/SegmentImage.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentImage.swift
+//  ClothesScannerTests
+//
+//  Created by Nicholas Kuo on 4/13/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+import Vision
+import CoreML
+
